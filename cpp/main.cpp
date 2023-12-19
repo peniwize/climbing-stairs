026424ec56e370c0a54eb58cda6c49ac8842f2cd
Matt Ervin <matt@impsoftware.org>
2023-12-18 22:05:44 -0800

    Refactored everything.  The previous code was insane.

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -11,6 +11,7 @@
 //!\sa https://github.com/doctest/doctest/blob/master/doc/markdown/main.md
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 
+#include <cassert>
 #include <doctest/doctest.h> //!\sa https://github.com/doctest/doctest/blob/master/doc/markdown/tutorial.md
 #include <iomanip>
 
@@ -21,106 +22,262 @@ using namespace std;
 class Solution {
 public:
     /*
-        01: 0 1 2 3 4
-            a
+        Problem:
 
-        02: 0_1 2 3 4 # First way to arrange 2..4; how many additional ways to arrange?
-              a b
-        03: 0_1 2_3 4
-              a   b c 
-        04: 0_1 2 3_4
-              a     b
-
-        05: 0 1_2 3 4 # First way to arrange 0, 3..4; how many additional ways to arrange?
-                a b
-        06: 0 1_2 3_4
-                a   b
-
-        07: 0 1 2_3 4 # First way to arrange 0..1, 4; how many additional ways to arrange?
-                  a b
-
-        08: 0 1 2 3_4 # First way to arrange 0..2; how many additional ways to arrange?
-                    a
-
-        Combine(array, i=0):
-            if 2 < len(array) && len(array) - 2 > i:
-                Combine the first pair in the array.
-                Combine(arrayp[i,])
-    */
-    int climbStairs(int n) {
-        if (0 == n) { return 0; }
+        You are climbing a staircase. It takes n steps to reach the top.
+        Each time you can either climb 1 or 2 steps.
+        In how many distinct ways can you climb to the top?
         
-        int count = 1;        
-        for (int i = 1; n > i; ++i) { // 'i' addresses the right-most item in the pair.
-            auto const remaining_items = n - i - 1;
-            if (0 < remaining_items) {
-                count += climbStairs(remaining_items);
-            } else {
-                ++count;
-            }
-        }
+        Solution:
+        
+        Find every combination of ways of taking steps.
+        Every valid combination will end on exactly the last step.
+        Count the number of ways the last step is reached.
+        This is the answer.
 
-        return count;
+        Example 1 (two steps):
+
+                +---+
+                | 2 |
+            +---+
+            | 1 |
+        ----+
+          0 (no step)
+
+        There are two ways to advance from step 0:
+            0 -> 1 (climb one step)
+            There is one [valid] way to advance from step 1:
+                1 -> 2 (climb one step)
+                * There is no way to advance; top step reached.
+                Taking two steps would overshoot step 2,
+                which is an invalid outcome since there is no third step!
+            0 -> 2 (climb two steps)
+            * There is no way to advance; top step reached.
+
+            The answer is the number of times step 2 was reached,
+            denoted with '*', which is two.
+
+            Decision trees are used to graph every combination in a solution set.
+            Note that the explanation above create this [horizontal] decision tree:
+
+                2
+               /
+              1
+             / \
+            0   x
+             \
+              2
+            
+            Each node in the tree represents a step on the staircase.
+            Starting at the landing (0), there are two ways to advance: 
+            chose one step or chose two steps.
+            Chosing one step leads to node 1.
+            Chosing two steps leads to node 2 (the final step).
+            From node 1 there are two ways to advance: 
+            chose one step or chose two steps.
+            Chosing one step leads to node 2 (the final step).
+            Chosing two steps leads to 'x' (an invalid outcome; because there is no third step).
+            The final step (2) can be reached two ways.
+
+        Patterns:
+
+            Note: a skipped step is joind to the following step with '+'
+
+            1 2 (take two steps)
+            1+2 (take one step)
+
+        Example 2 (four steps):
+
+                        +---+
+                        | 4 |
+                    +---+
+                    | 3 |
+                +---+
+                | 2 |
+            +---+
+            | 1 |
+        ----+
+          0 (no step)
+        
+        There are two ways to advance from step 0:
+            0 -> 1 (climb one step)
+            There are two ways to advance from step 1:
+                1 -> 2 (climb one step)
+                There are two ways to advance from step 2:
+                    2 -> 3 (climb one step)
+                    There is one way to advance from step 3:
+                        3 -> 4 (climb one step)
+                        * There is no way to advance; top step reached.
+                    2 -> 4 (climb two steps)
+                    * There is no way to advance; top step reached.
+                1 -> 3 (climb two steps)
+                There is one way to advance from step 3:
+                    3 -> 4 (climb one step)
+                    * There is no way to advance; top step reached.
+            0 -> 2 (climb two steps)
+            There are two ways to advance from step 2:
+                2 -> 3 (climb one step)
+                There is one way to advance from step 3:
+                    3 -> 4 (climb one step)
+                    * There is no way to advance; top step reached.
+                2 -> 4 (climb two steps)
+                * There is no way to advance; top step reached.
+
+            The answer is the number of times step 4 was reached,
+            denoted with '*', which is five.
+
+            The [vertical] decision tree for the explanation above is:
+            Note: the decision tree could also be horizontal.
+
+                            0
+                     /             \
+                    1               2
+                 /     \         /     \
+                2       3       3       4
+               / \     / \     / \
+              3   4   4   x   4   x
+             / \
+            4   x
+            
+            Branches that end with 'x' are invalid combinations, 
+            which do not produce a valid result, and are therefore uncounted.
+            The final step (4) can be reached five ways.
+
+        Patterns:
+
+            Note: a skipped step is joind to the following step with '+'
+
+            1 2 3 4
+            1+2 3 4
+            1+2 3+4
+            1 2+3 4
+            1 2 3+4
+    
+        The examples above show how all combinations of all ways of taking
+        a step are produced.
+
+        Time = O(2^n) == number of decision tree nodes visited.
+        Space = O(n) == number of levels of recursion.
+    */
+    int climbStairs_BruteForce_Recursive(int const n, int currentStep) {
+        if (n == currentStep) {
+            return 1;
+        } else if (n < currentStep) {
+            return 0; // Invalid branch/outcome; went beyone final step.
+        }
+        
+        // Advance from node in the decision tree by taking either:
+        return climbStairs_BruteForce_Recursive(n, currentStep + 1)    // one step
+               + climbStairs_BruteForce_Recursive(n, currentStep + 2); // or two steps.
+        // Only choices that ultimately lead to the final step 
+        // will produce a value > 0.  The result of each choice is 
+        // summed because all banches in the decision tree must be
+        // fully explored.
     }
 
-    /*
-        Combine(array, i=0):
-            if 2 < len(array) && len(array) - 2 > i:
-                Combine the first pair in the array.
-                Combine(arrayp[i,])
+    int climbStairs_BruteForce(int const n) {
+        return climbStairs_BruteForce_Recursive(n, 0);
+    }
+
+    /*!
+        This implementation is similar to 
+        'climbStairs_BruteForce_Recursive(int const n, int currentStep)',
+        except that it eliminates the 'currentStep' parameter by traveling
+        DOWN the steps to the landing rather than traveling UP the steps to
+        the stop step.
+
+        Time = O(2^n) == number of decision tree nodes visited.
+        Space = O(n) == number of levels of recursion.
     */
-    int climbStairsWithLogging(int n, int const prev_i = -1) {
-        if (0 == n) { return 0; }
+    int climbStairs_Backward_BruteForce_Recursive(int const n) {
+        if (0 == n) {
+            return 1;
+        } else if (0 > n) {
+            return 0; // Invalid branch/outcome; went beyone final step.
+        }
         
-        // First sequence is always discrete uncombined steps, e.g [0 1 2 3 4]
-        auto const is_top_level_call = -1 == prev_i;
-        int const sub_seq_idx = prev_i + 1;
-        int line_number = prev_i + 2;
-        int count = 1;
+        // Advance from node in the decision tree by taking either:
+        return climbStairs_Backward_BruteForce_Recursive(n - 1)    // one step
+               + climbStairs_Backward_BruteForce_Recursive(n - 2); // or two steps.
+        // Only choices that ultimately lead to the final step 
+        // will produce a value > 0.  The result of each choice is 
+        // summed because all banches in the decision tree must be
+        // fully explored.
+    }
 
-        // Print remainder of combination.
-        if (is_top_level_call) {
-            cerr << dec << setw(2) << line_number << ": ";
-        }
-        for (int j = 0; n > j; ++j) {
-            if (0 < j) { cerr << ' '; }
-            cerr << dec << (sub_seq_idx + j);
-        }
-        cerr << '\n';
-        ++line_number;
+    /*!
+        This implementation is similar to 
+        'climbStairs_Backward_BruteForce_Recursive(int const n)',
+        except that it uses memoization to aviod redundantly reevaluating 
+        steps (nodes in the decision tree) that were previously evaluated.
+
+        Time = O(2n) => O(n) == number of decision tree levels.
+        Space = O(O(n) + O(n+1)) => O(2n) => O(n) ==
+                number of [decision tree] levels of recursion + cache size.
+    */
+    int climbStairs_Backward_Memoized_Recursive(int const n, int cache[]) {
+        int result = -1;
         
-        for (int i = 1; n > i; ++i) { // 'i' addresses the right-most item in the pair.
-            // Print line number.
-            if (is_top_level_call) {
-                cerr << dec << setw(2) << (line_number) << ": ";
-            } else {
-                cerr << "    ";
-            }
-            // Print prefix gap.
-            for (int j = 0; sub_seq_idx > j; ++j) {
-                cerr << '.' << '.';
-            }
-            // Print first part of new combination.
-            for (int j = 0; i > j; ++j) {
-                if (0 < j) { cerr << ' '; }
-                cerr << dec << (sub_seq_idx + j);
-            }
-            cerr << '_' << dec << (sub_seq_idx + i) << ' ';
+        if (0 == n) {
+            result = 1;
+        } else if (0 > n) {
+            result = 0; // Invalid branch/outcome; went beyone final step.
+        } else if (-1 != cache[n]) {
+            result = cache[n];
+        } else {
+            // Advance from node in the decision tree by taking either:
+            result = climbStairs_Backward_Memoized_Recursive(n - 1, cache)   // one step
+                    + climbStairs_Backward_Memoized_Recursive(n - 2, cache); // or two steps.
+            // Only choices that ultimately lead to the final step 
+            // will produce a value > 0.  The result of each choice is 
+            // summed because all banches in the decision tree must be
+            // fully explored.
             
-            auto const old_count = count;
-            
-            auto const remaining_items = n - i - 1;
-            if (0 < remaining_items) {
-                count += climbStairsWithLogging(remaining_items, sub_seq_idx + i);
-            } else {
-                ++count;
-                cerr << '\n';
-            }
-
-            line_number += count - old_count;
+            assert(-1 == cache[n] || result == cache[n]);
+            cache[n] = result;
         }
 
-        return count;
+        return result;
+    }
+
+    int climbStairs_Backward_Memoized(int const n) {
+        int cache[n + 1];
+        std::fill_n(cache, n + 1, -1);
+        return climbStairs_Backward_Memoized_Recursive(n, cache);
+    }
+
+    /*!
+        This implementation is similar to:
+        'climbStairs_Backward_Memoized(int const n)',
+        except that it emliminates the need for a cache array.
+        This implementation also climbs the stairs forward and 
+        is exactly how Fibonacci numbers are calculated.
+
+        Time = O(n-2) => O(n)
+        Space = O(1)
+    */
+    int climbStairs_Optimized(int n) {
+        if (0 > n) {
+            return 0;
+        } else if (2 > n) {
+            return 1;
+        }
+
+        int n_minus_two = 1; // Initial value for the second step.
+        int n_minus_one = 1; // Initial value for the second step.
+        int result = 2;
+        --n; // Initial values assume first two steps taken.
+        --n; // Initial values assume first two steps taken.
+        while (0 < n--) {
+            n_minus_two = n_minus_one;
+            n_minus_one = result;
+            result = n_minus_one + n_minus_two;
+        }
+        return result;
+    }
+
+    int climbStairs(int n) {
+        return climbStairs_Optimized(n);
     }
 };
 
@@ -130,7 +287,7 @@ TEST_CASE("Case 0")
 {
     auto solution = Solution{};
     cerr << "Case 0" << '\n';
-    CHECK(0 == solution.climbStairs(0));
+    CHECK(1 == solution.climbStairs(0));
     cerr << '\n';
 }
 
@@ -171,6 +328,14 @@ TEST_CASE("Case 5")
     auto solution = Solution{};
     cerr << "Case 5" << '\n';
     CHECK(8 == solution.climbStairs(5));
+    cerr << '\n';
+}
+
+TEST_CASE("Case 45")
+{
+    auto solution = Solution{};
+    cerr << "Case 45" << '\n';
+    CHECK(1836311903 == solution.climbStairs(45));
     cerr << '\n';
 }
 
